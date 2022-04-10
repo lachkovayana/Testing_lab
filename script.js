cfc0ef95a9d022e36163bf568c01e699889092ff
Yana Lachkova <lyana688@gmail.com>
2022-04-10 16:48:36 +0700

    Integration test

---
+++ script.js
@@ -0,0 +1,80 @@
+import { countSmileys } from './task.js'
+
+const url = "http://192.168.56.1:8191/task"
+const elemList = document.querySelector('#elem_list')
+
+const init = () => {
+    document.getElementById("btn_save").addEventListener('click', createElement)
+    document.getElementById("btn_clean").addEventListener('click', deleteRequest)
+    loadTasks()
+}
+
+export async function postRequest(url, data) {
+    try {
+        const response = await fetch(url, {
+            headers: new Headers({
+                "Content-Type": "application/json",
+            }),
+            credentials: "same-origin",
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+        return await response.json()
+    } catch (error) {
+        return console.error(error)
+    }
+}
+
+export async function deleteRequest() {
+    try {
+        await fetch(url, {
+            method: "DELETE",
+        }).then(() => loadTasks())
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export async function getRequest() {
+    try {
+        const response = await fetch(url)
+        return await response.json()
+    } catch (error) {
+        return console.error(error)
+    }
+}
+
+function loadTasks() {
+    getRequest()
+        .then((tasksArray) => {
+            while (elemList.firstChild) {
+                elemList.removeChild(elemList.firstChild);
+            }
+            tasksArray.forEach(element => {
+                addElementOnPage(element)
+            })
+        })
+}
+
+function createElement() {
+    let input = document.querySelector("input#text").value
+    let result = countSmileys(input.split(", "))
+    let data = { input, result }
+    postRequest(url, data)
+        .then(response => console.log(response))
+
+}
+
+function addElementOnPage(element) {
+    const newElement = document.createElement("li")
+    newElement.textContent = generateText(element.input, element.result)
+    elemList.appendChild(newElement)
+}
+
+const generateText = (text, count) => {
+    return `Among faces "${text}" ${count} smiling`
+}
+
+init()
+
--- task.test.js
+++ task.test.js
@@ -1,68 +1,93 @@
-const countSmileys = require('./task');
+import {countSmileys, createNewElementDB} from "./task";
+import supertest from "supertest"
+const request = supertest("http://localhost:8191/")
 
-describe("Valid values", () => {
-  test('Basic combinations of eyes, noses and mouths', () => {
-    expect(countSmileys([':)', ':D', ';-D', ':~)'])).toBe(4);
-  });
 
-  test('Faces with valid noses', () => {
-    expect(countSmileys([';-D', ';~D', ';-('])).toBe(2);
-  });
+test('Create a POST request to /task', async() => {
+  const input = ":), :(, :-D"
+  const taskData = {
+      input,
+      result: countSmileys(input.split(", "))
+  }
 
-  test('Faces without noses', () => {
-    expect(countSmileys([';D', ';D', ';('])).toBe(2);
-  });
+  const newTask = await request
+    .post("task")
+    .send(taskData);
 
-  test('Unicode input', () => {
-    expect(countSmileys(['\u003a\u002d\u0029', '\u003a\u002d\u0028'])).toBe(1);
-  });
-})
+  expect(newTask.body).toHaveProperty("id");
+  expect(newTask.body.input).toBe(":), :(, :-D");
+  expect(newTask.statusCode).toBe(200);
+  // console.log(newTask.body);
+});
 
-describe("Invalid values", () => {
-  test('Some types of invalid values', () => {
-    expect(countSmileys([';(', ':-*', ':>', ':}', ';]', ':$', ':/', ':o'])).toBe(0);
-  });
 
-  test('Incorrect order of face elements', () => {
-    expect(countSmileys([')-:', '(:', '(:'])).toBe(0);
-  });
 
-  test('Faces with invalid noses', () => {
-    expect(countSmileys([':/D', ';*D', ';o(', ';oD'])).toBe(0);
-  });
+
+
+
+
+// ------------------------- <unit tests> -------------------------
+// describe("Valid values", () => {
+//   test('Basic combinations of eyes, noses and mouths', () => {
+//     expect(countSmileys([':)', ':D', ';-D', ':~)'])).toBe(4);
+//   });
+
+//   test('Faces with valid noses', () => {
+//     expect(countSmileys([';-D', ';~D', ';-('])).toBe(2);
+//   });
+
+//   test('Faces without noses', () => {
+//     expect(countSmileys([';D', ';D', ';('])).toBe(2);
+//   });
+
+//   test('Unicode input', () => {
+//     expect(countSmileys(['\u003a\u002d\u0029', '\u003a\u002d\u0028'])).toBe(1);
+//   });
+// })
+
+// describe("Invalid values", () => {
+//   test('Some types of invalid values', () => {
+//     expect(countSmileys([';(', ':-*', ':>', ':}', ';]', ':$', ':/', ':o'])).toBe(0);
+//   });
+
+//   test('Incorrect order of face elements', () => {
+//     expect(countSmileys([')-:', '(:', '(:'])).toBe(0);
+//   });
+
+//   test('Faces with invalid noses', () => {
+//     expect(countSmileys([':/D', ';*D', ';o(', ';oD'])).toBe(0);
+//   });
   
-  test('Faces with invalid eyes', () => {
-    expect(countSmileys(['%-D', '>-D', '!~(', '/~D'])).toBe(0);
-  });
+//   test('Faces with invalid eyes', () => {
+//     expect(countSmileys(['%-D', '>-D', '!~(', '/~D'])).toBe(0);
+//   });
   
-  test('Faces with invalid mouths', () => {
-    expect(countSmileys([':-=', ':-!', ';~|', ';~?'])).toBe(0);
-  });
-})
+//   test('Faces with invalid mouths', () => {
+//     expect(countSmileys([':-=', ':-!', ';~|', ';~?'])).toBe(0);
+//   });
+// })
 
-describe("Both valid and invalid faces", () => {
-  test('Should return 1 valid (against 4 invalid)', () => {
-    expect(countSmileys([';*', ':$', ';-D', ';(', ':>'])).toBe(1);
-  });
+// describe("Both valid and invalid faces", () => {
+//   test('Should return 1 valid (against 4 invalid)', () => {
+//     expect(countSmileys([';*', ':$', ';-D', ';(', ':>'])).toBe(1);
+//   });
 
-  test('Should return 4 valid (against 3 invalid)', () => {
-    expect(countSmileys([';D', ';-D', ';~)', ':~D', ':~(', ';oD', ';('])).toBe(4);
-  });
-})
+//   test('Should return 4 valid (against 3 invalid)', () => {
+//     expect(countSmileys([';D', ';-D', ';~)', ':~D', ':~(', ';oD', ';('])).toBe(4);
+//   });
+// })
 
-describe("Wrong input", () => {
-  test('Empty elements', () => {
-    expect(countSmileys(['', ' '])).toBe(0);
-  });
+// describe("Wrong input", () => {
+//   test('Empty elements', () => {
+//     expect(countSmileys(['', ' '])).toBe(0);
+//   });
   
-  test('Some strings values', () => {
-    expect(countSmileys([':', '-', ')', '-)',')-)','::)', 'abc'])).toBe(0);
-  });
+//   test('Some strings values', () => {
+//     expect(countSmileys([':', '-', ')', '-)',')-)','::)', 'abc'])).toBe(0);
+//   });
 
-  test('Wrong types', () => {
-    expect(countSmileys([1, [1, 2, 3], (1, 2), () => 123], null, undefined)).toBe(0);
-  });
+//   test('Wrong types', () => {
+//     expect(countSmileys([1, [1, 2, 3], (1, 2), () => 123], null, undefined)).toBe(0);
+//   });
   
-})
-
-
+// })
